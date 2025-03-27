e8f351ab302df84149029331f966e310c8aa6bac
fegger@ducksify.com <fegger@ducksify.com>
2025-03-27 09:03:56 +0100

    (bugfix) update documentation

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -1,5 +1,5 @@
 provider "panop" {
   skip_tls_verify = true
-  host            = "tower.panop.dev"
-  access_key      = "T2WDxZlp3s3BR4IehrlLcBqLP2rbd3C9bJQ58Leu7f879637"
+  host            = ""
+  access_key      = ""
 }
--- examples/provider/versions.tf
+++ examples/provider/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     panop = {
       source  = "panop-io/panop"
-      version = "0.1.1"
+      version = "0.1.2"
     }
   }
 }
